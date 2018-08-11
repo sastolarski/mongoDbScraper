5725e8150dc8cede18a02aacbe18bda711d29b74
sastolarski <sastolarski@gmail.com>
2018-08-11 08:23:18 -0500

    a

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ db.once("open", function () {
 
 
 //Routes
+//GET requests to render Handlebars pages
 app.get("/", function(req, res) {
     Article.find({"saved": false}, function(error, data) {
       var hbsObject = {
@@ -48,6 +49,124 @@ app.get("/", function(req, res) {
       console.log(hbsObject);
       res.render("homePage", hbsObject);
     });
+  });
+  
+  app.get("/saved", function(req, res) {
+    Article.find({"saved": true}).populate("notes").exec(function(error, articles) {
+      var hbsObject = {
+        article: articles
+      };
+      res.render("savedArticles", hbsObject);
+    });
+  });
+  
+  // A GET request to scrape the echojs website
+  app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with request
+    request("https://www.nytimes.com/", function(error, response, html) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(html);
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article").each(function(i, element) {
+  
+        // Save an empty result object
+        var result = {};
+  
+        // Add the title and summary of every link, and save them as properties of the result object
+        result.title = $(this).children("h2").text();
+        result.summary = $(this).children(".summary").text();
+        result.link = $(this).children("h2").children("a").attr("href");
+  
+        // Using our Article model, create a new entry
+        // This effectively passes the result object to the entry (and the title and link)
+        var entry = new Article(result);
+  
+        // Now, save that entry to the db
+        entry.save(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          // Or log the doc
+          else {
+            console.log(doc);
+          }
+        });
+  
+      });
+          res.send("Scrape Complete");
+  
+    });
+    // Tell the browser that we finished scraping the text
+  });
+  
+  // This will get the articles we scraped from the mongoDB
+  app.get("/articles", function(req, res) {
+    // Grab every doc in the Articles array
+    Article.find({}, function(error, doc) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Or send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
+  });
+  
+  // Grab an article by it's ObjectId
+  app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    Article.findOne({ "_id": req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    // now, execute our query
+    .exec(function(error, doc) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
+  });
+  
+  
+  // Save an article
+  app.post("/articles/save/:id", function(req, res) {
+        // Use the article id to find and update its saved boolean
+        Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true})
+        // Execute the above query
+        .exec(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          else {
+            // Or send the document to the browser
+            res.send(doc);
+          }
+        });
+  });
+  
+  // Delete an article
+  app.post("/articles/delete/:id", function(req, res) {
+        // Use the article id to find and update its saved boolean
+        Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": false, "notes": []})
+        // Execute the above query
+        .exec(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          else {
+            // Or send the document to the browser
+            res.send(doc);
+          }
+        });
   });
 
 
